03a0b6134fe416775615336f9f2784455776ac7a
miaopanlong <1730882258@qq.com>
2021-05-20 23:50:59 +0800

    version3

--- GitTest/src/HelloPrint.java
+++ GitTest/src/HelloPrint.java
@@ -2,5 +2,6 @@ public class HelloPrint {
     public static void main(String[] args) {
         System.out.println("this is version1,Hello Word!");
         System.out.println("this is version1,Hello XiaoMiao!");
+        System.out.println("this is version1,Hello XiaoLiu!");
     }
 }
